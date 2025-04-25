514c1a8db650ed4187c61041dff0e5cd0e97381d
russ1anyk <gustavogrelhus@gmail.com>
2025-04-25 16:01:52 -0300

    alterei as classes

--- src/app/components/Footer/footer.jsx
+++ src/app/components/Footer/footer.jsx
@@ -1,13 +1,11 @@
-import './footer.css'
+import './footer.css';
+
 export default function Footer() {
     return (
-        <>
-            <footer>
-                <div className="footer-content">
-                    <p>&copy; 2025 Barbearia G2. Todos os direitos reservados.</p>
-                </div>
-            </footer>
-
-        </>
-    )
+        <footer className="g2-footer">
+            <div className="g2-footer-content">
+                <p>&copy; 2025 Barbearia G2. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    );
 }
